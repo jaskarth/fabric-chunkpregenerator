45ed5d054d12be8395964763481cb82920036b29
SuperCoder7979 <k.pranav@gmail.com>
2020-09-24 09:21:38 -0400

    add go vote screen

--- src/main/java/supercoder79/chunkpregen/ChunkPregen.java
+++ src/main/java/supercoder79/chunkpregen/ChunkPregen.java
@@ -1,8 +1,11 @@
 package supercoder79.chunkpregen;
 
+import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import supercoder79.chunkpregen.client.GoVote;
 
 public final class ChunkPregen implements ModInitializer {
 	public static final Logger LOGGER = LogManager.getLogger(ChunkPregen.class);
@@ -10,5 +13,9 @@ public final class ChunkPregen implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		Commands.init();
+
+		if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+			GoVote.init();
+		}
 	}
 }
